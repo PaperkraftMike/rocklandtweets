8062ba83a96ded4ece31957c41e72ca1638262fb
Michael Cahill <miccahill@gmail.com>
2013-07-08 00:08:14 -0400

    added Zach's advice

--- rocklandtweets.rb
+++ rocklandtweets.rb
@@ -8,7 +8,9 @@ Twitter.configure do |config|
   config.oauth_token = "18829543-yUNWrIn3z0HaiEAo1D8PHBWRmRxWaX8MfbnSosFp4"
   config.oauth_token_secret = "5CeCXHzrpW8b7nPl4ncWZGW1oZVpZG2YMslFO2dE"
 end
+                    
 
 get '/' do
-	haml :index
+	@search = Twitter.search("rockland county", count: 10, result_type: "recent")  
+  haml :index
 end
